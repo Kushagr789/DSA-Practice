06608f089fa64089b2b7e482c6d0668758e23393
Kushagra Saxena <113609782+Kushagr789@users.noreply.github.com>
2024-04-25 00:06:08 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (89.62%) - LeetSync

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxDepth(String s) {
+        int c=0;
+        int max=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='(')
+            c++;
+            if(s.charAt(i)==')')
+            c--;
+            max=Math.max(c,max);
+        }
+        return max;
+
+        
+    }
+}
