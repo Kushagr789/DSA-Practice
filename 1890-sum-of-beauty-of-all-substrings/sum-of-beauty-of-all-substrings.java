4142319165b80731556b2d427be603ba16fd471d
Kushagra Saxena <113609782+Kushagr789@users.noreply.github.com>
2024-04-26 01:45:06 +0530

    Time: 59 ms (75.60%) | Memory: 42.4 MB (82.06%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -3,19 +3,20 @@ class Solution {
         int ans=0;
         for(int i=0;i<s.length();i++)
         {
-            HashMap<Character,Integer> map=new HashMap<>();
+            int[] c=new int[26];
             for(int j=i;j<s.length();j++)
             {
                 char ch=s.charAt(j);
-                if(map.containsKey(ch))
-                map.put(ch,map.get(ch)+1);
-                else
-                map.put(ch,1);
+                c[ch-'a']++;
                 int max=Integer.MIN_VALUE;
                 int min=Integer.MAX_VALUE;
-                for (Map.Entry entry : map.entrySet()) {
-                    min=Math.min(min,(int)entry.getValue());
-                        max=Math.max(max,(int)entry.getValue());
+                for(int x=0;x<26;x++)
+                {
+                    if(c[x]>0)
+                    {
+                        min=Math.min(min,c[x]);
+                        max=Math.max(max,c[x]);
+                    }
                 }
                 ans+=(max-min);
             }
