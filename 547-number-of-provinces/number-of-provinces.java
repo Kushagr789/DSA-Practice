424ef43cdb5ab0b8dfa84f7567ef7002672651af
Kushagra Saxena <113609782+Kushagr789@users.noreply.github.com>
2024-07-09 09:49:25 +0530

    Time: 0 ms (100.00%) | Memory: 47.9 MB (28.46%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,32 +1,23 @@
 class Solution {
-    static void dfs(int n,int[] vis,ArrayList<ArrayList<Integer>>adj){
+    static void dfs(int n,int[] vis,int[][] isConnected,int V){
         vis[n]=1;
-        for(Integer it:adj.get(n)){
-            if(vis[it]==0){
-                dfs(it,vis,adj);
+        for(int i=0;i<V;i++){
+            if(vis[i]==0&&isConnected[n][i]==1&&i!=n){
+                dfs(i,vis,isConnected,V);
             }
         }
     }
     public int findCircleNum(int[][] isConnected) {
-        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        
         int V=isConnected.length;
-        for(int i=0;i<V;i++)
-            adj.add(new ArrayList<>());
-        for(int i=0;i<V;i++){
-            for(int j=0;j<V;j++){
-                if(isConnected[i][j]==1&&i!=j){
-                    adj.get(i).add(j);
-                    adj.get(j).add(i);
-                }
-            }
-        }
+        
         int[] vis=new int[V];
         int cnt=0;
         for(int i=0;i<V;i++)
         {
             if(vis[i]!=1){
             cnt++;
-            dfs(i,vis,adj);
+            dfs(i,vis,isConnected,V);
             }
         }
         return cnt;
