9593dca7d7913e1f23c45723ddfcd876def019d4
Kushagra Saxena <113609782+Kushagr789@users.noreply.github.com>
2024-07-09 12:24:07 +0530

    Time: 1 ms (43.10%) | Memory: 45.2 MB (6.98%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,28 +1,42 @@
 class Solution {
-    private void dfs(int row, int col, 
-     int[][] ans,
-     int[][] image, 
-     int newColor, int delRow[], int delCol[],
-     int iniColor) {
-        ans[row][col] = newColor; 
+    
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        if (image == null || image.length == 0 || sr < 0 || sc < 0) {
+            return image;
+        }
+
+        int iniColor = image[sr][sc];
+        if (iniColor == color) {
+            return image;
+        }
+
+        int[][] ans = image;
         int n = image.length;
-        int m = image[0].length; 
-        for(int i = 0;i<4;i++) {
-            int nrow = row + delRow[i]; 
-            int ncol = col + delCol[i]; 
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol < m && image[nrow][ncol] == iniColor && ans[nrow][ncol] != newColor) {
-                dfs(nrow, ncol, ans, image, newColor, delRow, delCol, iniColor); 
+        int m = image[0].length;
+        
+        int[] delRow = {-1, 0, 1, 0};
+        int[] delCol = {0, 1, 0, -1};
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{sr, sc});
+        ans[sr][sc] = color;
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int row = current[0];
+            int col = current[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nrow = row + delRow[i];
+                int ncol = col + delCol[i];
+
+                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && image[nrow][ncol] == iniColor && ans[nrow][ncol] != color) {
+                    ans[nrow][ncol] = color;
+                    queue.add(new int[]{nrow, ncol});
+                }
             }
         }
-    }
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        if(image==null||image.length==0||sr<0||sc<0)return image;
-        int iniColor = image[sr][sc]; 
-        int[][] ans = image; 
-        int dx[] = {-1, 0, +1, 0};
-        int dy[] = {0, +1, 0, -1}; 
-        dfs(sr, sc, ans, image, color, dx, dy, iniColor); 
+
         return ans;
-        
     }
 }
