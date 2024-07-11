f76ff2a170268c7825986a4a53d44831e8d3f3e5
Kushagra Saxena <113609782+Kushagr789@users.noreply.github.com>
2024-07-11 09:16:59 +0530

    Time: 5 ms (99.75%) | Memory: 55.9 MB (65.62%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,49 +1,37 @@
 class Solution {
+    static int n,m;
+    static void dfs(int r,int c,int[][]grid){
+        if(r<0||r>=n||c<0||c>=m)
+            return;
+        if(grid[r][c]==0) return;
+        grid[r][c]=0;
+        dfs(r,c+1,grid);
+        dfs(r,c-1,grid);
+        dfs(r+1,c,grid);
+        dfs(r-1,c,grid);
+    }
     public int numEnclaves(int[][] grid) {
-        int n=grid.length;
-        int m=grid[0].length;
-        Queue<Pair> q=new LinkedList<>();
+        n=grid.length;
+        m=grid[0].length;
         int[][] vis=new int[n][m];
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(i==0||i==n-1||j==0||j==m-1){
-                    if(grid[i][j]==1){
-                        q.add(new Pair(i,j));
-                        vis[i][j]=1;
-                    }
-                }
-            }
+            if(grid[i][0]==1)
+                dfs(i,0,grid);
+            if(grid[i][m-1]==1)
+                dfs(i,m-1,grid);
         }
-        int[] dr={0,0,1,-1};
-        int[] dc={1,-1,0,0};
-        while(!q.isEmpty()){
-            int r=q.peek().first;
-            int c=q.peek().second;
-            q.remove();
-            for(int i=0;i<4;i++){
-                int nr=r+dr[i];
-                int nc=c+dc[i];
-                if(nr>=0&&nr<n&&nc>=0&&nc<m&&grid[nr][nc]==1&&vis[nr][nc]==0){
-                    vis[nr][nc]=1;
-                    q.add(new Pair(nr,nc));
-                }
-            }
+        for(int i=0;i<m;i++){
+            if(grid[0][i]==1)
+                dfs(0,i,grid);
+            if(grid[n-1][i]==1)
+                dfs(n-1,i,grid);
         }
         int cnt=0;
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1&&vis[i][j]==0)
-                    cnt++;
-            }
+            for(int j=0;j<m;j++)
+                cnt+=grid[i][j];
         }
+    
         return cnt;
-    }
-}
-class Pair{
-    int first;
-    int second;
-    Pair(int r,int c){
-        this.first=r;
-        this.second=c;
     }
 }
