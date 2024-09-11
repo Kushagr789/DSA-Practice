1c931edf11869b59e4b71ca4cf46e224d0055e9b
Kushagra Saxena <113609782+Kushagr789@users.noreply.github.com>
2024-09-11 23:01:25 +0530

    Time: 25 ms (35.08%) | Memory: 44.4 MB (53.95%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,25 +1,27 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
-        int[][] dp=new int[n][amount+1];
+        int[] dp=new int[amount+1];
+        int[] curr=new int[amount+1];
         for (int i = 0; i <= amount; i++) {
             if (i % coins[0] == 0)
-                dp[0][i] = i / coins[0];
+                dp[i] = i / coins[0];
             else
-                dp[0][i] = (int) Math.pow(10, 9);
+                dp[i] = (int) Math.pow(10, 9);
         }
         for(int i=1;i<n;i++){
             for(int j=0;j<=amount;j++){
                 int take=(int)1e9;
-                int notTake=dp[i-1][j];
+                int notTake=dp[j];
                 if(coins[i]<=j){
-                    take=1+dp[i][j-coins[i]];
+                    take=1+curr[j-coins[i]];
                 }
-                dp[i][j]=Math.min(take,notTake);
+                curr[j]=Math.min(take,notTake);
             }
+            dp=curr;
         }
-        if(dp[n-1][amount]<(int)1e9)
-        return dp[n-1][amount];
+        if(dp[amount]<(int)1e9)
+        return dp[amount];
         else
         return -1;
     }
