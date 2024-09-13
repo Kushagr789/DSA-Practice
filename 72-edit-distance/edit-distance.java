6b53443e3f3217fae5bcf7fa9f79e36434fdd1da
Kushagra Saxena <113609782+Kushagr789@users.noreply.github.com>
2024-09-13 23:50:25 +0530

    Time: 5 ms (66.22%) | Memory: 44.6 MB (89.20%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -2,27 +2,28 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int n=word1.length();
         int m=word2.length();
-        int[][] dp=new int[n+1][m+1];
-        for(int i=0;i<=n;i++){
-            dp[i][0]=i;
-        }
+        if(n==0&&m>0)
+        return 1;
+        int[] dp=new int[m+1];
         for(int j=0;j<=m;j++){
-            dp[0][j]=j;
+            dp[j]=j;
         }
-
+        int[] curr=new int[m+1];
         for(int i=1;i<=n;i++){
+            curr[0]=i;
             for(int j=1;j<=m;j++){
                 if(word1.charAt(i-1)==word2.charAt(j-1)){
-                    dp[i][j]=dp[i-1][j-1];
+                    curr[j]=dp[j-1];
                 }else{
-                    dp[i][j]=Math.min(
-                        dp[i][j-1],
-                        Math.min(dp[i-1][j],
-                        dp[i-1][j-1])
+                    curr[j]=Math.min(
+                        curr[j-1],
+                        Math.min(dp[j],
+                        dp[j-1])
                     )+1;
                 }
             }
+            dp=curr.clone();
         }
-        return dp[n][m];
+        return curr[m];
     }
 }
