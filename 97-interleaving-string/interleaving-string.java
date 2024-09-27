07b787491e90aa3c69ff3c456050b49483fed53c
Kushagra Saxena <113609782+Kushagr789@users.noreply.github.com>
2024-09-27 20:38:32 +0530

    Time: 2 ms (71.78%) | Memory: 41.2 MB (96.46%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -5,24 +5,24 @@ class Solution {
         int n3=s3.length();
         if(n1+n2!=n3)
         return false;
-        boolean[][] dp=new boolean[n1+1][n2+1];
-        dp[0][0]=true;
-        for(int i=1;i<=n1;i++){
-            dp[i][0]=(dp[i-1][0]&&(s1.charAt(i-1)==s3.charAt(i-1)));
-        }
+        boolean[] dp=new boolean[n2+1];
+        boolean[] curr=new boolean[n2+1];
+        
+        dp[0]=true;
         for(int j=1;j<=n2;j++){
-            dp[0][j]=(dp[0][j-1]&&(s2.charAt(j-1)==s3.charAt(j-1)));
+            dp[j]=(dp[j-1]&&(s2.charAt(j-1)==s3.charAt(j-1)));
         }
 
         for(int i=1;i<=n1;i++){
+            dp[0]=dp[0]&&s1.charAt(i-1)==s3.charAt(i-1);
             for(int j=1;j<=n2;j++){
-                dp[i][j]=
-                (dp[i-1][j]&&s1.charAt(i-1)==s3.charAt(i+j-1))
+                dp[j]=
+                (dp[j]&&s1.charAt(i-1)==s3.charAt(i+j-1))
                 ||
-                (dp[i][j-1]&&s2.charAt(j-1)==s3.charAt(i+j-1));
+                (dp[j-1]&&s2.charAt(j-1)==s3.charAt(i+j-1));
             }
-
+            
         }
-        return dp[n1][n2];
+        return dp[n2];
     }
 }
